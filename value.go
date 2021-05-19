dded8157b01a26fe30739c2dd8683e8aba565ff7
wuchenhao <18310828498@163.com>
2021-05-19 09:12:30 +0800

    字符串转数字

--- value.go
+++ value.go
@@ -1,11 +1,22 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var  a = "RUNOOB"
+	var a = "RUNOOB"
 	fmt.Println(a)
 	var b int
 	fmt.Println(b)
 	var c error
 	fmt.Println(c)
+
+	var st string = "120q"
+	num, err := strconv.Atoi(st) //字符串转数字
+	if err == nil {
+		fmt.Println(err)
+	}
+	fmt.Println(num)
 }
